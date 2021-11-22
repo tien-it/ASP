c6a9cbbc8c3019d55a835e64e5e1dedb3bf54b34
tien-it <69241666+tien-it@users.noreply.github.com>
2021-11-22 08:16:51 +0700

    account
    account

---
+++ doanasp/doanasp/Data/ShopContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using doanasp.Models;
+namespace doanasp.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        { }
+        //public DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ doanasp/doanasp/Models/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace doanasp.Models
+{
+    public class account
+    {
+        public int id { get; set; }
+
+        [DisplayName("Tên đăng nhập")]
+        public string username { get; set; }
+        [DisplayName("Mật Khẩu")]
+        public String password { get; set; }
+        [DisplayName("Email")]
+        public String email { get; set; }
+        [DisplayName("Họ và Tên")]
+        public String fullname { get; set; }
+        [DisplayName("Địa Chỉ")]
+        public String permanent_address { get; set; }
+        [DisplayName("SĐT")]
+        public String phonenumber { get; set; }
+        [DisplayName("Ảnh")]
+        public String avatar { get; set; }
+        [DisplayName("Admininstrator")]
+        public int type_user { get; set; }
+    }
+}
