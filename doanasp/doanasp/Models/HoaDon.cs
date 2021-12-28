5af0afe70e38d40030a130bd9f165f8593bf9e4a
lelongtoan <83998616+lelongtoan@users.noreply.github.com>
2021-12-28 13:16:46 +0700

    update_02

--- doanasp/doanasp/Models/HoaDon.cs
+++ doanasp/doanasp/Models/HoaDon.cs
@@ -10,24 +10,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace doanasp.Models
 {
     public class HoaDon
-<<<<<<< Updated upstream
-    {
-        [Key]
-        [DisplayName("MÃ HÓA ĐƠN")]
-        public int MAHOADON { get; set; }
-        [DisplayName("MÃ KHÁCH HÀNG")]
-        public int MAKHACHHANG { get; set; }
-        [DisplayName("SỐ ĐIỆN THOẠI")]
-        public String SDT { get; set; }
-        [DisplayName("ĐỊA CHỈ")]
-        public String DIACHI { get; set; }
-        [DisplayName("GHI CHÚ")]
-        public String GHICHU { get; set; }
-        [DisplayName("NGÀY LẬP")]
-        public DateTime NGAYLAP { get; set; }
-        [DisplayName("CẬP NHẬT")]
-        public DateTime NGAYGIAO { get; set; }
-=======
     {  
         [Key]
         [DisplayName("MÃ HÓA ĐƠN")]
@@ -49,8 +31,6 @@ namespace doanasp.Models
         
         [DisplayName("NGÀY GIAO")]
         public DateTime NGAYGIAO { get; set; }
-        
->>>>>>> Stashed changes
         [DisplayName("TỔNG TIỀN")]
         public int TONGTIEN { get; set; }
         //trang thái hóa đơn : 0 đang xử lý , 1 đã tiếp nhận, 2 đang giao, 4 là giao hàng thành công, -1 bị hủy 
